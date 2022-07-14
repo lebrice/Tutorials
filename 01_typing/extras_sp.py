9c7509d7006714034114b233009cdf04278a8a7d
Fabrice Normandin <fabrice.normandin@gmail.com>
2022-07-14 19:13:00 -0400

    make sp_example functional
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- 01_typing/extras_sp.py
+++ 01_typing/extras_sp.py
@@ -1,15 +1,29 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from simple_parsing import ArgumentParser
 from simple_parsing.helpers.hparams.hparam import uniform, log_uniform
 from simple_parsing.helpers.hparams.hyperparameters import HyperParameters
 
 
 @dataclass
 class ModelHparams(HyperParameters):
+    """Model Hyper-Parameters."""
 
     # Optimizer learning rate
     lr: float = uniform(1e-6, 1e-3, default=1e-4)
 
 
 hp = ModelHparams.sample()
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_arguments(ModelHparams, "hparams")
+    args = parser.parse_args()
+    hp: ModelHparams = args.hparams
+    print(hp)
+
+if __name__ == "__main__":
+    main()
+    
